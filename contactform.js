f574ba021c1b730fd3050b3d30852d2ea583aea6
Russell Shea <russell2shea@gmail.com>
2019-08-01 22:28:15 -0400

    played with animejs

--- contactform.js
+++ contactform.js
@@ -1,49 +1,26 @@
-var current = null;
-document.querySelector('#email').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: 0,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#password').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -336,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '240 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
-document.querySelector('#submit').addEventListener('focus', function(e) {
-  if (current) current.pause();
-  current = anime({
-    targets: 'path',
-    strokeDashoffset: {
-      value: -730,
-      duration: 700,
-      easing: 'easeOutQuart'
-    },
-    strokeDasharray: {
-      value: '530 1386',
-      duration: 700,
-      easing: 'easeOutQuart'
-    }
-  });
-});
+function contactForm(){
+
+  document.addEventListener("DOMContentLoaded", function(){
+    var current = null;
+    var submitBtn = document.querySelectorAll('.submit');
+
+    submitBtn[0].addEventListener("click", function(){
+      console.log('hey'); 
+      if (current) current.pause();
+
+      current = anime({
+        targets: '.contact_intro',
+        height: '0px',
+        easing: 'easeInOutQuad'
+      });
+
+
+      
+
+    });
+  }); 
+
+} // conactForm
+
+
+contactForm();
