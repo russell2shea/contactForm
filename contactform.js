bb0f559334131248bcb34e8c299af9c0ce125d04
Russell Shea <russell2shea@gmail.com>
2019-08-06 21:30:54 -0400

    added success styles

--- contactform.js
+++ contactform.js
@@ -1,26 +1,31 @@
-function contactForm(){
-
-  document.addEventListener("DOMContentLoaded", function(){
-    var current = null;
-    var submitBtn = document.querySelectorAll('.submit');
-
-    submitBtn[0].addEventListener("click", function(){
-      console.log('hey'); 
-      if (current) current.pause();
-
-      current = anime({
-        targets: '.contact_intro',
-        height: '0px',
-        easing: 'easeInOutQuad'
-      });
 
 
-      
+document.addEventListener("DOMContentLoaded", function(){
+  
+  const card = document.querySelector('.card');
+  card.addEventListener( 'click', function() {
+    //prevent default, run try catch of post then..
+    card.classList.toggle('is-flipped');
+    //if success
 
-    });
-  }); 
+    const statusElement = document.querySelector('.status');
+    const messageElement = document.querySelector('.message');
 
-} // conactForm
+    statusElement.innerHTML = '<div class="success"><p>✓</p></div>';
+    messageElement.innerHTML = '<div class="success-message">Message Sent</div>';
+  });
 
 
-contactForm();
+
+});// when ready 
+
+
+
+
+
+
+// add whe click do animation and show yes button 
+// success btn click toggles back to submit 
+// do same but make a error button
+// add listener to when all 3 field are done to add class to submit button
+// add try catch and submit call to tie into submit animations
